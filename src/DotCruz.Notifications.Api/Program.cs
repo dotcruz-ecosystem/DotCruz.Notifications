ef80a97d4ccb750f10b8fb85be63c2188fe0856d
Matheus Felipe <matheus.felipescruz@gmail.com>
2026-08-17 14:48:24 -0300

    feat(security): allow anonymous access to openapi docs in development

--- src/DotCruz.Notifications.Api/Program.cs
+++ src/DotCruz.Notifications.Api/Program.cs
@@ -24,14 +24,12 @@ builder.Services.AddCrossCutting(builder.Configuration);
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapOpenApi().AllowAnonymous();
+    app.MapScalarApiReference().AllowAnonymous();
 }
 
 app.UseHttpsRedirection();
